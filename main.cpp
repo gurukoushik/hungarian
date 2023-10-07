c257598612ac4b988fc06791954313867e84e581
Guru Senthil <guru.koushik@gmail.com>
2023-10-07 12:43:23 -0700

    feat: add compute time

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+
 #include "hungarian.hpp"
 
 int main() {
@@ -8,12 +10,21 @@ int main() {
                                                   {13, 13, 15, 13, 14},
                                                   {18, 10, 13, 16, 14},
                                                   {12, 8, 16, 19, 10}};
+
+  auto timeStart = std::chrono::high_resolution_clock::now();
+
   taskassignment t1(cost_matrix, 5);
   std::vector<std::vector<int>> assignment = t1.hungarian();
+
+  auto timeEnd = std::chrono::high_resolution_clock::now();
+  auto timeTaken = std::chrono::duration_cast<std::chrono::duration<double>>(
+      timeEnd - timeStart);
 
   std::cout << "The optimal assignment is: \n\n";
   t1.print_matrix(assignment);
   std::cout << std::endl;
+  std::cout << "Time taken to compute: " << timeTaken.count() << " s"
+            << std::endl;
 
   return 0;
 }
