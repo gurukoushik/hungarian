0e289dce588f9c3db393272a9fc252cf18b8fd77
Guru Senthil <guru.koushik@gmail.com>
2023-10-07 12:37:37 -0700

    fix: move include to header

--- hungarian.hpp
+++ hungarian.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <limits>
 #include <list>
+#include <queue>
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#include <queue>
-
 #include "hungarian.hpp"
 
 int main() {
