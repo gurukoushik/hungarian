a59ff18bfe8aeb04c5472858692a9590f500dff5
Guru Senthil <guru.koushik@gmail.com>
2023-10-06 22:45:14 -0700

    fix: fmt

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main() {
   std::cout << "\nHungarian Method for nxn optimal assignment\n\n";
 
   std::vector<std::vector<double>> cost_matrix = {{11, 7, 10, 17, 10},
-                                        {13, 21, 7, 11, 13},
-                                        {13, 13, 15, 13, 14},
-                                        {18, 10, 13, 16, 14},
-                                        {12, 8, 16, 19, 10}};
+                                                  {13, 21, 7, 11, 13},
+                                                  {13, 13, 15, 13, 14},
+                                                  {18, 10, 13, 16, 14},
+                                                  {12, 8, 16, 19, 10}};
   taskassignment t1(cost_matrix, 5);
   std::vector<std::vector<int>> assignment = t1.hungarian();
 
