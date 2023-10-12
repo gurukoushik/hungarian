55042ea0063417b5913485e3932f3ade3750cda2
Guru Senthil <guru.koushik@gmail.com>
2023-10-11 22:15:10 -0700

    test: add test for templating

--- src/tests/test_hungarian.cpp
+++ src/tests/test_hungarian.cpp
@@ -19,3 +19,56 @@ BOOST_AUTO_TEST_CASE(test_3x3) {
     }
   }
 }
+
+BOOST_AUTO_TEST_CASE(test_templating) {
+  // double
+  {
+    std::vector<std::vector<double>> cost_matrix = {
+        {1.0, 2.0, 3.0}, {3.0, 1.0, 2.0}, {2.0, 3.0, 1.0}};
+
+    TaskAssignment<double> t1(cost_matrix);
+    std::vector<std::vector<int>> assignment = t1.hungarian();
+    std::vector<std::vector<int>> expected_assignment = {
+        {1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+    for (int i = 0; i < cost_matrix.size(); i++) {
+      for (int j = 0; j < cost_matrix[0].size(); j++) {
+        BOOST_REQUIRE_MESSAGE(assignment[i][j] == expected_assignment[i][j],
+                              "Fail " << i << " " << j);
+      }
+    }
+  }
+
+  // float
+  {
+    std::vector<std::vector<float>> cost_matrix = {
+        {1.0, 2.0, 3.0}, {3.0, 1.0, 2.0}, {2.0, 3.0, 1.0}};
+
+    TaskAssignment<float> t1(cost_matrix);
+    std::vector<std::vector<int>> assignment = t1.hungarian();
+    std::vector<std::vector<int>> expected_assignment = {
+        {1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+    for (int i = 0; i < cost_matrix.size(); i++) {
+      for (int j = 0; j < cost_matrix[0].size(); j++) {
+        BOOST_REQUIRE_MESSAGE(assignment[i][j] == expected_assignment[i][j],
+                              "Fail " << i << " " << j);
+      }
+    }
+  }
+
+  // int
+  {
+    std::vector<std::vector<int>> cost_matrix = {
+        {1, 2, 3}, {3, 1, 2}, {2, 3, 1}};
+
+    TaskAssignment<int> t1(cost_matrix);
+    std::vector<std::vector<int>> assignment = t1.hungarian();
+    std::vector<std::vector<int>> expected_assignment = {
+        {1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+    for (int i = 0; i < cost_matrix.size(); i++) {
+      for (int j = 0; j < cost_matrix[0].size(); j++) {
+        BOOST_REQUIRE_MESSAGE(assignment[i][j] == expected_assignment[i][j],
+                              "Fail " << i << " " << j);
+      }
+    }
+  }
+}
